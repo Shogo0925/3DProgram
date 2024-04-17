ca29cf37d05bd28566399b0b71341ba7e8993a2f
Shogo0925 <Kd1374207@st.kobedenshi.ac.jp>
2024-04-17 10:52:42 +0900

    修正
    絶対消すを削除

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,8 +221,6 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
-	//絶対消す
-	m_test = 5;
 
 	return true;
 }
